2e50b2677b0be7ca549eca868388ac762bb5b7b9
Barkin Unal <barkinunal@gmail.com>
2025-07-24 16:26:43 +0100

    parserState

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,13 +1,16 @@
 package request
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 )
 
 type Request struct {
 	RequestLine RequestLine
+	parserState parserState
 }
 
 type RequestLine struct {
@@ -16,47 +19,112 @@ type RequestLine struct {
 	Method        string
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	buf, err := io.ReadAll(reader)
-	requestLine, err := parseRequestLine(string(buf))
+type parserState int
 
-	if err != nil {
-		return nil, errors.New("Error parsing the request line")
+const (
+	initialised parserState = iota
+	done
+)
+
+const CRLF = "\r\n"
+const BUFFER_SIZE = 8
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := &Request{
+		parserState: initialised,
+	}
+	buffer := make([]byte, BUFFER_SIZE, BUFFER_SIZE)
+	readToIndex := 0
+
+	for request.parserState != done {
+		if readToIndex >= len(buffer) {
+			newBuffer := make([]byte, len(buffer)*2)
+			copy(newBuffer, buffer)
+			buffer = newBuffer
+		}
+
+		numBytesRead, err := reader.Read(buffer[readToIndex:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				request.parserState = done
+				break
+			}
+			return nil, err
+		}
+
+		readToIndex += numBytesRead
+
+		numBytesParsed, err := request.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buffer, buffer[numBytesParsed:])
+		readToIndex -= numBytesParsed
 	}
 
-	return &Request{
-		RequestLine: *requestLine,
-	}, nil
+	return request, nil
 }
 
-func parseRequestLine(buf string) (*RequestLine, error) {
-	lines := strings.Split(buf, "\r\n")
-	requestLine := lines[0]
-
-	attributes := strings.Split(requestLine, " ")
-	if len(attributes) < 3 {
-		return nil, errors.New("Not enough attributes in request line")
+func (request *Request) parse(data []byte) (int, error) {
+	numBytesConsumed, requestLine, err := parseRequestLine(data)
+	if err != nil {
+		return 0, err
 	}
 
-	// method
-	if attributes[0] != strings.ToUpper(attributes[0]) {
-		return nil, errors.New("Method is not all uppercase.")
+	if numBytesConsumed != 0 {
+		request.RequestLine = *requestLine
+		request.parserState = done
 	}
 
-	// target
-	if (len(attributes[1]) > 0 && attributes[1][0] != '/') || (len(attributes[1]) > 1 && attributes[1][1:] != strings.ToLower(string(attributes[1][1:]))) {
-		return nil, errors.New("Request target isn't all lowercase")
+	return numBytesConsumed, nil
+}
+
+func parseRequestLine(data []byte) (int, *RequestLine, error) {
+	idx := bytes.Index(data, []byte(CRLF))
+	if idx == -1 {
+		return 0, nil, nil
+	}
+	requestLineText := string(data[:idx])
+	requestLine, err := requestLineFromString(requestLineText)
+	if err != nil {
+		return 0, nil, err
+	}
+	return idx, requestLine, nil
+}
+
+func requestLineFromString(str string) (*RequestLine, error) {
+	parts := strings.Split(str, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("poorly formatted request-line: %s", str)
 	}
 
-	// version
-	if attributes[2] != "HTTP/1.1" {
-		return nil, errors.New("HTTP version needs to be HTTP/1.1")
+	method := parts[0]
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return nil, fmt.Errorf("invalid method: %s", method)
+		}
+	}
+
+	requestTarget := parts[1]
+
+	versionParts := strings.Split(parts[2], "/")
+	if len(versionParts) != 2 {
+		return nil, fmt.Errorf("malformed start-line: %s", str)
+	}
+
+	httpPart := versionParts[0]
+	if httpPart != "HTTP" {
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", httpPart)
+	}
+	version := versionParts[1]
+	if version != "1.1" {
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", version)
 	}
 
 	return &RequestLine{
-		Method:        attributes[0],
-		RequestTarget: attributes[1],
-		HttpVersion:   "1.1",
+		Method:        method,
+		RequestTarget: requestTarget,
+		HttpVersion:   versionParts[1],
 	}, nil
-
 }
